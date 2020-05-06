5afcf8766e3358fc11d74c3ae2aabb4046f96e9d
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-06 15:52:39 +0900

    실습일지 7번 예제 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -228,6 +228,24 @@ namespace cscondition
             {
                 Console.WriteLine("가을");
             }
+
+            // 실습일지 7번 예제 실습
+            Console.Write("학년을 입력하세요 : ");
+            int level = int.Parse(Console.ReadLine());
+
+            switch (level)
+            {
+                case 1:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 12학점");
+                    break;
+                case 2:
+                case 4:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
+                    break;
+                case 3:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 10학점");
+                    break;
+            }
         }
     }
 }
