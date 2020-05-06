c820e7e3a13efa88740cf764d5090041db2e4d6c
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-06 15:42:56 +0900

    실습일지 5번 예제 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -165,6 +165,49 @@ namespace cscondition
             {
                 Console.WriteLine("조건에 맞습니다.");
             }
+
+            // 실습일지 5번 예제 실습
+            Console.Write("\n\n태어난 년도 입력 : ");
+            int year = int.Parse(Console.ReadLine());
+            switch(year % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이");
+                    break;
+                case 1:
+                    Console.WriteLine("닭");
+                    break;
+                case 2:
+                    Console.WriteLine("개");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐");
+                    break;
+                case 5:
+                    Console.WriteLine("소");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼");
+                    break;
+                case 8:
+                    Console.WriteLine("용");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀");
+                    break;
+                case 10:
+                    Console.WriteLine("말");
+                    break;
+                case 11:
+                    Console.WriteLine("양");
+                    break;
+            }
         }
     }
 }
